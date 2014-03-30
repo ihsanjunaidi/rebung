651f185e97392d18c03bfab7d9d2280d6612a949
Ihsan Junaidi Ibrahim <ihsanjunaidi@users.noreply.github.com>
2014-03-31 05:04:07 +0800

    Create main.go

---
+++ rdbtool/main.go
@@ -0,0 +1,146 @@
+/*
+ * Copyright (c) 2013 Ihsan Junaidi Ibrahim <ihsan@n3labs.my>
+ */
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/garyburd/redigo/redis"
+	"os"
+)
+
+type SessionKey struct {
+	MsgIdNext    string
+	ServerIdNext string
+
+	NServerAll      int
+	NServerEnabled  int
+	NServerDisabled int
+	NServerActive   int
+	NServerInactive int
+}
+
+const (
+	APPNAME = "rdbtool"
+	APPVER  = "0.1"
+)
+
+var rdb redis.Conn
+
+func flush(db int) (err error) {
+	if _, err = rdb.Do("select", db); err != nil {
+		return
+	}
+
+	if _, err = rdb.Do("flushdb"); err != nil {
+		return
+	}
+
+	return
+}
+
+func checkRedis(url, pw string) (err error) {
+	if rdb == nil {
+		rdb, err = redis.Dial("tcp", url)
+		if err != nil {
+			return
+		}
+
+		if _, err = rdb.Do("auth", pw); err != nil {
+			return
+		}
+	}
+
+	return
+}
+
+func main() {
+	var (
+		err      error
+		debug    bool
+		help     bool
+		redisUrl = "dev1.int.n3labs.my:6379"
+		redisPw  = "d0raemon"
+		redisDb  int
+	)
+
+	flag.BoolVar(&debug, "d", true, "Debug mode")
+	flag.BoolVar(&help, "h", false, "Display usage")
+	flag.Parse()
+
+	if help {
+		usage()
+	}
+
+	if len(flag.Args()) > 3 {
+		usage()
+	}
+
+	err = checkRedis(redisUrl, redisPw)
+	if err != nil {
+		fatal(err.Error())
+	}
+	defer rdb.Close()
+
+	f := flag.Args()
+	if f[0] == "session" {
+		redisDb = 1
+		switch f[1] {
+		case "flush":
+			err = flush(redisDb)
+		case "reset":
+			err = resetSession(redisDb)
+		case "dump":
+			err = dumpSession(redisDb)
+		case "stat":
+			err = statSession(redisDb)
+
+		default:
+			fatal("Invalid command")
+		}
+	} else if f[0] == "user" {
+		redisDb = 2
+		switch f[1] {
+		case "flush":
+			err = flush(redisDb)
+		case "reset":
+			err = resetUser(redisDb)
+		case "dump":
+			err = dumpUser(redisDb)
+		case "exist":
+			err = existUser(redisDb, f[2])
+
+		default:
+			fatal("Invalid command")
+		}
+	} else {
+		fatal("Invalid operation")
+	}
+
+	if err != nil {
+		fatal(err.Error())
+	}
+}
+
+func fatal(a string, v ...interface{}) {
+	var n string
+
+	if len(v) != 0 {
+		n = fmt.Sprintf(a, v...)
+	} else {
+		n = a
+	}
+
+	fmt.Fprintf(os.Stderr, "Fatal: "+n+"\n")
+	os.Exit(1)
+}
+
+func usage() {
+	str := fmt.Sprintf("%v-%v\nusage: %v [-d] [-h] op cmd arg\n", APPNAME,
+		APPVER, APPNAME)
+
+	fmt.Fprintf(os.Stderr, str)
+	os.Exit(1)
+}
